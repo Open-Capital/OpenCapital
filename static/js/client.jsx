427da32a0b971208167a0c8517c9f7a32023ba4e
Noah Santacruz <noahssantacruz@gmail.com>
2017-07-18 11:34:26 +0300

    reverting and trying git blame on s2

--- static/js/client.jsx
+++ static/js/client.jsx
@@ -1,3 +1,10 @@
+var $            = require('jquery'),
+    React        = require('react'),
+    ReactDOM     = require('react-dom'),
+    DjangoCSRF   = require('./lib/django-csrf'),
+    SefariaReact = require('./s2');
+
+
 $(function() {
   var container = document.getElementById('s2');
   var component;
--- static/js/client1.jsx
+++
@@ -1,5 +0,0 @@
-var $            = require('jquery'),
-    React        = require('react'),
-    ReactDOM     = require('react-dom'),
-    DjangoCSRF   = require('./lib/django-csrf'),
-    SefariaReact = require('./s2');
